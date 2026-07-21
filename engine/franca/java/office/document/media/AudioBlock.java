f2fa577a3bba926809e7af7778144cd4ff8072f4
sergeev-i-leo <sergeev-i-leo@gmail.com>
2026-07-21 11:23:27 +0300

    commit

--- engine/franca/java/office/document/media/AudioBlock.java
+++ engine/franca/java/office/document/media/AudioBlock.java
@@ -1,11 +1,18 @@
 package franca.java.office.document.media;
 
+import franca.java.data.json.JsonObject;
 import franca.java.expected.BufferedString;
 import franca.java.office.document.Block;
 
 public class AudioBlock extends Block {
 
   public String source = "";
+
+  @Override
+  public void fillJsonObject(JsonObject jsonObject) {
+    super.fillJsonObject(jsonObject);
+    jsonObject.putStringValue("source", source);
+  }
 
   @Override
   public void serialize(BufferedString targetBufferedString, int spacesBefore) {
